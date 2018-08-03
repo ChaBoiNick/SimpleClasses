057b187203bc7e295070ae77d701aafa2f895677
Nicholas Schmidt <talkschmidt@hotmail.com>
2018-08-03 14:09:05 +1000

    Car classes

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -10,6 +10,36 @@ namespace SimpleClasses
     {
         static void Main(string[] args)
         {
+            Car myCar = new Car();
+            myCar.Make = "Holden";
+            myCar.Model = "Commodore";
+            myCar.Year = 2008;
+            myCar.Colour = "Blue";
+
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Colour);
+            Console.WriteLine("{0:C}",myCar.DetermineMarketValue());
+
+            Console.ReadLine();
         }
+
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 2000;
+            return carValue;
+        }
+
+
     }
 }
